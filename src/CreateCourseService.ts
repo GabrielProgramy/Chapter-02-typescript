fa3565a631706e4cf3aead07c38c7b1c1d7c415c
Gabriel Costa <gabrielestevan14@gmail.com>
2021-03-29 21:19:15 -0300

    working with typing

---
+++ src/CreateCourseService.ts
@@ -0,0 +1,14 @@
+interface Course {
+  name: string;
+  duration: number;
+  educator: string;
+}
+
+
+class CreateCoursesService {
+  execute({ name, duration, educator }: Course) {
+    console.log(name, duration, educator);
+  }
+}
+
+export default new CreateCoursesService();
